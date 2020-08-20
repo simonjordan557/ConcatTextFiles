669e6128ddca8287d7f29e28591085248e580ec2
simonjordan557 <simonjordan557@aol.com>
2020-08-21 00:48:37 +0100

    Uploaded program files.

---
+++ ConcatTextFiles/ConcatTextFiles/Play.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace ConcatTextFiles
+{
+    class Play
+    {
+        List<string> toWrite = new List<string>();
+        string writePath;
+
+        public void Go(string path)
+        {
+            try
+            {
+                writePath = path + @"\results.txt";
+                string[] directoryList = Directory.GetDirectories(path);
+
+
+
+                Console.WriteLine($"Number of directories: {directoryList.Length}.");
+
+                if (directoryList.Length == 0)
+                {
+                    ExtractTxtFileContentsFromDirectory(path);
+                }
+
+                else
+                {
+                    foreach (var directory in directoryList)
+                    {
+                        ExtractTxtFileContentsFromDirectory(directory);
+                    }
+                }
+
+                if (File.Exists(writePath))
+                {
+                    File.Delete(writePath);
+                }
+
+                foreach (var line in toWrite)
+                {
+
+                    Console.WriteLine(line);
+
+                    
+
+                    using (StreamWriter sw = new StreamWriter(writePath, true))
+                    {
+                        sw.WriteLine(line);
+                    }
+                }
+
+                using (StreamWriter sw = new StreamWriter(writePath, true))
+                {
+                    sw.WriteLine($"\n\n**** FILE COMPLETE. THANKS FOR CONCATENATING! ****");
+                }
+
+
+
+
+            }
+
+            catch (Exception e)
+            {
+                Console.Clear();
+                Console.WriteLine($"PROGRAM FAILED! INVALID PATH.\n\n" + e.Message);
+                Console.WriteLine("\nEnter a valid path: ");
+                path = Console.ReadLine();
+                Go(path);
+            }
+
+            
+        }
+
+        private void ExtractTxtFileContentsFromDirectory(string path)
+        {
+            string[] fileList = Directory.GetFiles(path, "*.txt");
+
+            foreach (var file in fileList)
+            {
+                if (file == writePath)
+                {
+                    continue;
+                }
+
+                toWrite.Add($"\n\n{file}\n");
+                using (StreamReader sr = new StreamReader(file))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        toWrite.Add(sr.ReadLine());
+                    }
+
+                    toWrite.Add("\n\n");
+                }
+
+
+            }
+        }
+        
+
+
+    }
+}
---
+++ ConcatTextFiles/ConcatTextFiles/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConcatTextFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n**** THIS PROGRAM CONCATENTATES .TXT FILES ****\n");
+                
+            Console.WriteLine("\nPlease enter the path of the source directory: ");
+
+            string path = Console.ReadLine();
+
+            Play play = new Play();
+            play.Go(path);
+
+            
+            Console.WriteLine("\n\n**** PROGRAM COMPLETE. PRESS ENTER TO FINISH. ****");
+            Console.ReadLine();
+
+
+        }
+    }
+}
